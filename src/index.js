32b53225075215d9c4c3a27479859b8a6e92d1c7
daniel-onuii <daniel@onuii.com>
2022-05-16 14:47:24 +0900

    fix: 전역 css 제거

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import styles from './styles.module.css';
 import TimeTable from './component/TimeTable';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 export const OnuiiTimeTable = ({ areaData, itemData }) => {
